bae34bf0abde11c24cd64e91754082c62c28be77
Gabriel-O-Nascimento <gabrielnacimento1011@gmail.com>
2026-04-21 19:12:48 -0300

    Criação e estilização do Header mobile

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,34 +1,47 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/logo.png";
+import { Menu } from 'lucide-react';
+import { CircleX } from 'lucide-react';
 
 const Header = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    return (
-        <header className="header">
-            <div className="container header-content">
+  return (
+    <header className="header">
+      <div className="container header-content">
 
-                {/* LOGO */}
-                <div className="logo">
-                    <Link to="/">
-                        <img src={Logo} alt="Desenrola Logo" />
-                    </Link>
-                </div>
-                {/* NAVEGAÇÃO */}
-                <nav className={`nav ${menuOpen ? "open" : ""}`}>
-                    <ul>
-                        <li><Link to="/">Início</Link></li>
-                        <li><Link to="/chat">Chat</Link></li>
-                        <li><Link to="/search">Pesquisar</Link></li>
-                        <li><Link to="/services">Serviços</Link></li>
-                        <li><Link to="/profile">Perfil</Link></li>
-                    </ul>
-                </nav>
-            </div>
+        {/* LOGO */}
+        <div className="logo">
+          <Link to="/">
+            <img src={Logo} alt="Desenrola Logo" />
+          </Link>
+        </div>
 
-        </header>
-    );
+        {/* BOTÃO MOBILE */}
+        <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+          <Menu size={32} stroke-width={2.2}/>
+        </button>
+
+        {/* NAV */}
+        <nav className={`nav ${menuOpen ? "open" : ""}`}>
+            <button className="close-menu" onClick={() => setMenuOpen(false)}>
+                <CircleX size={32} stroke-width={2}/>
+            </button>
+
+          <ul>
+            <li><Link to="/" onClick={() => setMenuOpen(false)}>Início</Link></li>
+            <li><Link to="/chat" onClick={() => setMenuOpen(false)}>Chat</Link></li>
+            <li><Link to="/search" onClick={() => setMenuOpen(false)}>Pesquisar</Link></li>
+            <li><Link to="/services" onClick={() => setMenuOpen(false)}>Serviços</Link></li>
+            <li><Link to="/profile" onClick={() => setMenuOpen(false)}>Perfil</Link></li>
+          </ul>
+        </nav>
+
+      </div>
+    </header>
+  );
 };
+
 
 export default Header;
